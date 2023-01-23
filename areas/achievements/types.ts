8a86911c41e58bb261a6061a4a2ec4ad06bd6674
Alan Walton <aawalton@gmail.com>
2023-01-22 18:13:57 -0700

    remove unused AchievementFilter type

--- areas/achievements/types.ts
+++ areas/achievements/types.ts
@@ -2,8 +2,6 @@ export type AchievementType = 'boolean' | 'integer' | 'collection' | 'sequence'
 
 export type AchievementPoints = '5' | '10' | '15' | '50'
 
-export type AchievementFilter = 'active' | 'passive' | 'all'
-
 export type AchievementFormatFilter = 'passive' | 'automatic' | 'focused'
 
 export type AchievementCircleFilter = 'solo' | 'jenny' | 'group'
